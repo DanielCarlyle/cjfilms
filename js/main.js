80259241eb2ea83006e7b5c0b0861dbbd7c260b8
dan carlyle <dancarlyle@dans-MacBook-Pro.local>
2020-04-22 18:39:28 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,71 @@
+$(document).ready(function () {
+
+
+	$("#portfolio-contant-active").mixItUp();
+
+
+	$("#testimonial-slider").owlCarousel({
+		paginationSpeed: 500,
+		singleItem: true,
+		autoPlay: 3000,
+	});
+
+
+
+
+	$("#clients-logo").owlCarousel({
+		autoPlay: 3000,
+		items: 5,
+		itemsDesktop: [1199, 5],
+		itemsDesktopSmall: [979, 5],
+	});
+
+	$("#works-logo").owlCarousel({
+		autoPlay: 3000,
+		items: 5,
+		itemsDesktop: [1199, 5],
+		itemsDesktopSmall: [979, 5],
+	});
+
+
+	// google map
+	var map;
+	function initMap() {
+		map = new google.maps.Map(document.getElementById('map'), {
+			center: { lat: -34.397, lng: 150.644 },
+			zoom: 8
+		});
+	}
+
+
+	// Counter
+
+	$('.counter').counterUp({
+		delay: 10,
+		time: 1000
+	});
+
+
+
+	// ===== Scroll to Top ==== 
+	$(window).scroll(function () {
+		if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+			$('#return-to-top').fadeIn(200);    // Fade in the arrow
+		} else {
+			$('#return-to-top').fadeOut(200);   // Else fade out the arrow
+		}
+	});
+	$('#return-to-top').click(function () {      // When arrow is clicked
+		$('body,html').animate({
+			scrollTop: 0                       // Scroll to top of body
+		}, 500);
+	});
+
+
+
+
+});
+
+
+
+
